0bc2c93d621b32e1dba6fd4076d81e6e841e4331
alexgerardojacinto <ag.jacinto@campus.fct.unl.pt>
2021-01-25 17:49:44 +0000

    fix: else if condition was checking the wrong case

--- src/android/com/outsystems/plugins/fileviewer/OSFileViewer.java
+++ src/android/com/outsystems/plugins/fileviewer/OSFileViewer.java
@@ -29,7 +29,7 @@ public class OSFileViewer extends CordovaPlugin {
             this.openDocumentFromLocalPath(args, callbackContext);
             return true;
         }
-        else if (action.equals(KEY_ACTION_PREVIEW_DOCUMENT_FROM_URL)) {
+        else if (action.equals(KEY_ACTION_OPEN_DOCUMENT_FROM_URL)) {
             this.openDocumentFromUrl(args, callbackContext);
             return true;
         }
