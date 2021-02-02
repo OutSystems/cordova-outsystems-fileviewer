163f5cde008d593da7e928c84498d7d28187bd09
alexgerardojacinto <ag.jacinto@campus.fct.unl.pt>
2021-02-02 14:37:19 +0000

    refactor: remove unused mimetype
    References: https://outsystemsrd.atlassian.net/browse/RMET-440

--- www/OSFileViewer.js
+++ www/OSFileViewer.js
@@ -12,8 +12,8 @@ exports.openDocumentFromLocalPath = function (filePath, fileMIMEType, success, e
     exec(success, error, 'OSFileViewer', 'openDocumentFromLocalPath', [filePath, fileMIMEType]);
 };
 
-exports.openDocumentFromUrl = function (url, fileMIMEType, success, error) {
-    exec(success, error, 'OSFileViewer', 'openDocumentFromUrl', [url, fileMIMEType]);
+exports.openDocumentFromUrl = function (url, success, error) {
+    exec(success, error, 'OSFileViewer', 'openDocumentFromUrl', [url]);
 };
 
 exports.previewMediaContent = function (url, fileMIMEType, success, error) {
