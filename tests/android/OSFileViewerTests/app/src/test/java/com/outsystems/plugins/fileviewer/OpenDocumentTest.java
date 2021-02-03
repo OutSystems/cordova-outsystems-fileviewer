4da22837d6cd289ad0444386077ff8dc30703d86
alexgerardojacinto <ag.jacinto@campus.fct.unl.pt>
2021-02-03 19:07:41 +0000

    feature: add tests for android platform
    References: https://outsystemsrd.atlassian.net/browse/RMET-440

---
+++ tests/android/OSFileViewerTests/app/src/test/java/com/outsystems/plugins/fileviewer/OpenDocumentTest.java
@@ -0,0 +1,28 @@
+package com.outsystems.plugins.fileviewer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class OpenDocumentTest {
+
+    @Test
+    public void isPathValid_FilePathSuccess(){
+
+        assertTrue(OSOpenDocument.getInstance().isPathValid("file:///storage/emulated/0/android/media/com.outsystems.rd.FileViewerTest/nomePDF.pdf"));
+
+    }
+
+    @Test
+    public void isPathValid_FilePathError(){
+
+        assertFalse(OSOpenDocument.getInstance().isPathValid(""));
+
+    }
+
+}
