ee3590f44f4b01b5935dc2d34864c987ef37134b
Carlos Corrêa <carlscorrea@gmail.com>
2021-02-05 16:02:18 +0000

    feature: unit tests url validation

--- src/ios/FileViewerErrors.swift
+++ src/ios/FileViewerErrors.swift
@@ -10,7 +10,7 @@ import Foundation
 enum FileViewerErrors: String, Error {
     case fileDoesNotExist = "The file does not exist"
     case couldNotOpenDocument = "Could not open the document"
-    case invalidURL = "Invalid URL"
+    case invalidURL = "The URL you are trying to open is malformed"
     case invalidEmptyURL = "Invalid empty URL"
 }
 
