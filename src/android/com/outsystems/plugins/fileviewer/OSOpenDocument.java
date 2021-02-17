c8278e7c820f8a7a28816857b12b649137c16a8a
André Grillo <andregrillo@gmail.com>
2021-02-17 18:18:21 +0000

    Imported Input and Output Stream libs

--- src/android/com/outsystems/plugins/fileviewer/OSOpenDocument.java
+++ src/android/com/outsystems/plugins/fileviewer/OSOpenDocument.java
@@ -11,6 +11,10 @@ import androidx.core.content.FileProvider;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
